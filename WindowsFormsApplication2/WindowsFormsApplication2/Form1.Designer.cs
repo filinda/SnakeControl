937a669e7afcfc953a1319d64edf9b73ef48b83e
filinda <42602257+filinda@users.noreply.github.com>
2019-05-17 13:47:49 +0300

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(833, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(867, 489);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(891, 546);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge;
+using AForge.Video.DirectShow;
+using AForge.Video;
+using System.IO;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        VideoCaptureDevice frame;
+        FilterInfoCollection Devices;
+
+        byte[] data = new byte[10];
+        bool moove = false;
+        byte xdirect = 125;
+        byte ydirect = 125;
+
+
+        SerialPort serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            data[0] = 15;
+            data[9] = 15;
+            serialPort = new SerialPort("COM6", 115200);
+            try
+            {
+                serialPort.Open();
+            }
+            catch (IOException e)
+            {
+            } timer1.Start();
+        }
+
+        void Start_cam()
+        {
+            Devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            frame = new VideoCaptureDevice(Devices[0].MonikerString);
+            frame.NewFrame += new AForge.Video.NewFrameEventHandler(NewFrame_event);
+            frame.Start();
+        }
+
+        string output;
+        void NewFrame_event(object send, NewFrameEventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = (Image)e.Frame.Clone();
+            }
+            catch (Exception ex) { }
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Up) {
+                ydirect = 155;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                xdirect = 155;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                xdirect = 95;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                ydirect = 95;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                moove = true;
+            }
+
+            if (e.KeyCode == Keys.F)
+            {
+                data[8] = 2;
+            }
+
+            if (e.KeyCode == Keys.G)
+            {
+                data[8] = 1;
+            }
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up &  ydirect == 155)
+            {
+                ydirect = 125;
+            }
+            if (e.KeyCode == Keys.Right & xdirect == 155)
+            {
+                xdirect = 125;
+            }
+            if (e.KeyCode == Keys.Left & xdirect == 95)
+            {
+                xdirect = 125;
+            }
+            if (e.KeyCode == Keys.Down & ydirect == 95)
+            {
+                ydirect = 125;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                moove = false;
+            }
+        }
+
+        float time = 0, last = 0;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            data[1] = xdirect;
+            data[2] = ydirect;
+
+            time = DateTime.Now.Millisecond+DateTime.Now.Second*1000;
+                label1.Text = "" + time;
+                if (moove)
+                {
+                    for (int i = 3; i < 8; i += 1)
+                    {
+                        data[i] = (byte)(125 + (50 * Math.Sin(time / 200.0 + i * 0.9)));
+                    }
+                }
+                else {
+                    for (int i = 3; i < 8; i += 2)
+                    {
+                        data[i] = (byte)(125);
+                    }
+                }
+
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Write(data, 0, 10);
+                }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start_cam();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Start_cam();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            frame.Stop();
+        }
+    }
+}
